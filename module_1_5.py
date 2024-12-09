fcd77f3a6dc810df76ce24030695c20846f19164
Rutatata <bikbaevrustam98@gmail.com>
2024-12-09 23:01:18 +0300

    module_1_5.py

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 'Rango', True, 75
+print(immutable_var)
+#immutable_var [0] = 2 (Нельзя изменять так как является кортежом)
+mutable_list = [1, 2, 'Dualingo', False, 2,5]
+print(mutable_list)
+mutable_list [2] = ('Lingonberry')
+print(mutable_list)
